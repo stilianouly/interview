50cbc61e5c2a7c676ec44aef7fe899afd99105eb
stilianouly <stilianos.menelaou@gmail.com>
2022-10-06 21:37:12 +1100

    Add decorate one frame rate repository with cache

--- forex-mtl/src/main/scala/forex/services/rates/Interpreters.scala
+++ forex-mtl/src/main/scala/forex/services/rates/Interpreters.scala
@@ -3,15 +3,20 @@ package forex.services.rates
 import cats.Applicative
 import cats.effect.Sync
 import forex.config.ApplicationConfig
-import forex.repository.oneframe.GetOneFrameDataRepository
+import forex.domain.Rate
+import forex.repository.RepositoryCache
+import forex.repository.oneframe.{GetOneFrameDataRepository, Protocol}
 import interpreters._
 import org.http4s.client.Client
+
+import scala.concurrent.duration.{Duration, MINUTES}
 
 object Interpreters {
   def dummy[F[_]: Applicative]: Algebra[F] = new OneFrameDummy[F]()
 
   def oneFrameRateInterpreter[F[_]: Sync](client: Client[F], config: ApplicationConfig): Algebra[F] = {
-    val getOneFrameDataRepository = new GetOneFrameDataRepository(client, config)
-    new OneFrameRateInterpreter(getOneFrameDataRepository.get(_))
+    val getOneFrameDataRepository: GetOneFrameDataRepository[F] = new GetOneFrameDataRepository(client, config)
+    val getOneFrameDataRepositoryCache: RepositoryCache[F, Rate.Pair, List[Protocol.OneFrameRateResponseData]] = new RepositoryCache(getOneFrameDataRepository.get(_), Duration(4.5, MINUTES))
+    new OneFrameRateInterpreter(getOneFrameDataRepositoryCache.get(_))
   }
 }
