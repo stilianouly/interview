b99a25cd4d321f79746a6ef88f023110ba67aee8
stilianouly <stilianos.menelaou@gmail.com>
2022-10-07 16:22:37 +1100

    Add caching behaviour to rates program

--- forex-mtl/src/main/scala/forex/programs/rates/Program.scala
+++ forex-mtl/src/main/scala/forex/programs/rates/Program.scala
@@ -1,11 +1,13 @@
 package forex.programs.rates
 
 import cats.data.EitherT
+import cats.implicits.{catsSyntaxApplicativeId, toFlatMapOps, toFunctorOps}
 import cats.{Applicative, Monad}
 import forex.domain._
 import forex.programs.rates.Program.getRequestedRate
 import forex.programs.rates.Protocol.GetRatesRequest
 import forex.programs.rates.errors._
+import forex.services.rates.errors.{Error => ServiceError}
 import forex.services.{RatesService, ValueCacheService}
 
 class Program[F[_]: Monad](
@@ -15,29 +17,27 @@ class Program[F[_]: Monad](
 
   override def get(request: Protocol.GetRatesRequest): F[Error Either Rate] = {
 
-    val oneFrameResponseProgram : F[Error Either Rate] = {
-      val maybeErrorOrRates: EitherT[F, Error, List[Rate]] = EitherT(
-        ratesService.get(Rate.Pair(request.from, request.to))
-      ).leftMap(toProgramError(_))
-
-      valueCacheService.cache(List())
-
-      val maybeErrorOrRate = maybeErrorOrRates.flatMap(rates => getRequestedRate(request, rates))
-      maybeErrorOrRate.value
+    val errorOrRatesProgram: F[Either[ServiceError, List[Rate]]] = valueCacheService.get.flatMap {
+      case Some(cachedRates) => Monad[F].pure(Right(cachedRates))
+      case None => {
+        ratesService.get(Rate.Pair(request.from, request.to)).flatMap {
+          case Right(ratesFromService) => valueCacheService.cache(ratesFromService).map(_ => Right(ratesFromService))
+          case error => error.pure[F]
+        }
+      }
     }
 
-    oneFrameResponseProgram
+    EitherT(errorOrRatesProgram).leftMap(toProgramError).flatMap(rates => getRequestedRate(request, rates)).value
   }
 }
 
 object Program {
-
   def apply[F[_] : Monad](
       ratesService: RatesService[F],
       valueCacheService: ValueCacheService[F, List[Rate]]
   ): Algebra[F] = new Program[F](ratesService, valueCacheService)
 
-  private def getRequestedRate[F[_] : Applicative](request: GetRatesRequest, rates: List[Rate]): EitherT[F, Error, Rate] = {
+  def getRequestedRate[F[_] : Applicative](request: GetRatesRequest, rates: List[Rate]): EitherT[F, Error, Rate] = {
     val maybeRequestedRate = rates.find(rate => request.to == rate.pair.to && request.from == rate.pair.from)
 
     EitherT.fromOption[F](maybeRequestedRate, Error.ResponseEmpty("Service returned a missing data set"))
---
+++ forex-mtl/src/test/scala/forex/programs/rates/ProgramTest.scala
@@ -0,0 +1,88 @@
+package forex.programs.rates
+
+import cats.effect.IO
+import forex.domain.Rate.Pair
+import forex.domain.{Currency, Price, Rate, Timestamp}
+import forex.programs.rates.Protocol.GetRatesRequest
+import forex.services.RatesService
+import forex.services.valuecache.{Algebra => CacheAlgebra}
+import org.scalatest.funspec.AnyFunSpec
+
+import java.time.OffsetDateTime
+
+class TestCache() extends CacheAlgebra[IO, List[Rate]] {
+  var cachedItem: List[Rate] = null
+
+  override def cache(value: List[Rate]): IO[Any] = IO{ cachedItem = value }
+
+  override def get: IO[Option[List[Rate]]] = IO(Option(cachedItem))
+}
+
+class ProgramTest extends AnyFunSpec {
+
+  val time = OffsetDateTime.parse("2022-01-11T07:48:09.94Z")
+
+  val rate1 = Rate(Pair(Currency.AUD, Currency.USD), Price(1.0), Timestamp(time))
+  val rate2 = Rate(Pair(Currency.USD, Currency.AUD), Price(2.0), Timestamp(time))
+  val rate3 = Rate(Pair(Currency.JPY, Currency.USD), Price(3.0), Timestamp(time))
+  val rate4 = Rate(Pair(Currency.USD, Currency.JPY), Price(4.0), Timestamp(time))
+  val rate5 = Rate(Pair(Currency.JPY, Currency.AUD), Price(5.0), Timestamp(time))
+  val rate6 = Rate(Pair(Currency.AUD, Currency.JPY), Price(6.0), Timestamp(time))
+
+  val rates = List(rate1, rate2, rate3, rate4, rate5, rate6)
+
+  describe("Program") {
+    describe("caching") {
+      describe("cache is empty") {
+        it("should get the requested rate from the rates service, then cache the rates") {
+
+          val ratesService: RatesService[IO] = _ => IO(Right(rates))
+
+          val testCache = new TestCache
+
+          val program = new Program[IO](ratesService, testCache)
+
+          val cachedItemBeforeRun = testCache.cachedItem
+
+          val result = program.get(GetRatesRequest(Currency.AUD, Currency.USD)).unsafeRunSync()
+
+          val cachedItemAfterRun = testCache.cachedItem
+
+          val expected = Right(rate1)
+
+          assert(result == expected)
+          assert(cachedItemBeforeRun == null)
+          assert(cachedItemAfterRun == rates)
+        }
+      }
+
+      describe("cache is not empty") {
+        it("should get the requested rate from the cache service, and not call the rates service") {
+
+          var ratesServiceCalled = "no"
+
+          val emptyRatesService: RatesService[IO] = _ => IO{ratesServiceCalled = "yes"; Right(Nil)}
+
+          val testCache = new TestCache
+
+          testCache.cachedItem = rates
+
+          val program = new Program[IO](emptyRatesService, testCache)
+
+          val cachedItemBeforeRun = testCache.cachedItem
+
+          val result = program.get(GetRatesRequest(Currency.AUD, Currency.USD)).unsafeRunSync()
+
+          val cachedItemAfterRun = testCache.cachedItem
+
+          val expected = Right(rate1)
+
+          assert(result == expected)
+          assert(cachedItemBeforeRun == rates)
+          assert(cachedItemAfterRun == rates)
+          assert(ratesServiceCalled == "no")
+        }
+      }
+    }
+  }
+}
